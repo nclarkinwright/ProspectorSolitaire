acc8c7a4fc01dc733b9d7619ad339285483b4dcf
nclarkinwright <49002688+nclarkinwright@users.noreply.github.com>
2020-11-14 15:56:28 -0500

    Cards built in code
    Getting NullReferenceException at Deck.cs:269

--- Assets/__Scripts/Card.cs
+++ Assets/__Scripts/Card.cs
@@ -4,7 +4,32 @@ using UnityEngine;
 
 public class Card : MonoBehaviour
 {
-    // This will be defined later
+    [Header("Set Dynamically")]
+    public string suit; // Suit of the Card (C, D, H or S)
+    public int rank; // Rank of the Card (1-14)
+    public Color color = Color.black; // Color to tint pips
+    public string colS = "Black"; // or "Red". Name of the Color
+
+    // This List holds all of the Decorator GameObjects
+    public List<GameObject> decoGOs = new List<GameObject>();
+    // This List holds all of the Pip GameObjects
+    public List<GameObject> pipGOs = new List<GameObject>();
+
+    public GameObject back; // The GameObject of the back of the card
+
+    public CardDefinition def; // Parsed from DeckXML.xml
+
+    public bool faceUp
+    {
+        get
+        {
+            return (!back.activeSelf);
+        }
+        set
+        {
+            back.SetActive(!value);
+        }
+    }
 }
 
 [System.Serializable] // A Serializable class is able to be edited in the Inspector
